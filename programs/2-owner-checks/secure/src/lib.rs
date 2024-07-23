74bb34a843f25ad06f3c756bebc1b19fa12689dc
wiseaidev <oss@wiseai.dev>
2024-07-23 13:02:39 +0300

    feat: upgrade program 2 to anchor latest

--- programs/2-owner-checks/secure/src/lib.rs
+++ programs/2-owner-checks/secure/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program::program_error::ProgramError;
-use anchor_lang::solana_program::program_pack::Pack;
+use spl_token::solana_program::program_pack::Pack;
 use spl_token::state::Account as SplTokenAccount;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
@@ -9,13 +9,13 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod owner_checks_secure {
     use super::*;
 
-    pub fn log_message(ctx: Context<LogMessage>) -> ProgramResult {
-        let token = SplTokenAccount::unpack(&ctx.accounts.token.data.borrow())?;
-        if ctx.accounts.token.owner != &spl_token::ID {
-            return Err(ProgramError::InvalidAccountData);
+    pub fn log_message(ctx: Context<LogMessage>) -> Result<()> {
+        let token = SplTokenAccount::unpack(&ctx.accounts.token.data.borrow()).unwrap();
+        if ctx.accounts.token.owner.to_string() != spl_token::ID.to_string() {
+            return Err(ProgramError::InvalidAccountData.into());
         }
-        if ctx.accounts.authority.key != &token.owner {
-            return Err(ProgramError::InvalidAccountData);
+        if ctx.accounts.authority.key.to_string() != token.owner.to_string() {
+            return Err(ProgramError::InvalidAccountData.into());
         }
         msg!("Your account balance is: {}", token.amount);
         Ok(())
