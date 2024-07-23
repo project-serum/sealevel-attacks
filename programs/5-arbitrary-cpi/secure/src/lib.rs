eecbb7d504800221e792498d1263d13561585527
wiseaidev <oss@wiseai.dev>
2024-07-23 13:14:11 +0300

    feat: upgrade program 5 to anchor latest

--- programs/5-arbitrary-cpi/secure/src/lib.rs
+++ programs/5-arbitrary-cpi/secure/src/lib.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_lang::solana_program;
+use anchor_lang::solana_program::system_instruction::transfer;
+use anchor_spl::token::spl_token;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -7,25 +9,22 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod arbitrary_cpi_secure {
     use super::*;
 
-    pub fn cpi_secure(ctx: Context<Cpi>, amount: u64) -> ProgramResult {
-        if &spl_token::ID != ctx.accounts.token_program.key {
-            return Err(ProgramError::IncorrectProgramId);
+    pub fn cpi_secure(ctx: Context<Cpi>, amount: u64) -> Result<()> {
+        if spl_token::ID.to_string() != ctx.accounts.token_program.key.to_string() {
+            return Err(ProgramError::IncorrectProgramId.into());
         }
-        solana_program::program::invoke(
-            &spl_token::instruction::transfer(
-                ctx.accounts.token_program.key,
+        Ok(solana_program::program::invoke(
+            &transfer(
                 ctx.accounts.source.key,
                 ctx.accounts.destination.key,
-                ctx.accounts.authority.key,
-                &[],
                 amount,
-            )?,
+            ),
             &[
                 ctx.accounts.source.clone(),
                 ctx.accounts.destination.clone(),
                 ctx.accounts.authority.clone(),
             ],
-        )
+        )?)
     }
 }
 
