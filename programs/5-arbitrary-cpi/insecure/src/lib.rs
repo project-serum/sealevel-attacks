c922175fe8ef0df913a52f04a4f8659d03e801ec
techvoyagerX <bytebender90@gmail.com>
2024-09-09 03:29:48 -0400

    Added comments to 5-arbitrary-cpi in secure, recommended, and insecure versions

--- programs/5-arbitrary-cpi/insecure/src/lib.rs
+++ programs/5-arbitrary-cpi/insecure/src/lib.rs
@@ -7,10 +7,12 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod arbitrary_cpi_insecure {
     use super::*;
 
+    // Insecure CPI invocation without program ID verification
     pub fn cpi(ctx: Context<Cpi>, amount: u64) -> ProgramResult {
+        // Directly invokes the `transfer` instruction from the token program without checking the legitimacy of the CPI
         solana_program::program::invoke(
             &spl_token::instruction::transfer(
-                ctx.accounts.token_program.key,
+                ctx.accounts.token_program.key,  // Unsafe: token program isn't verified
                 ctx.accounts.source.key,
                 ctx.accounts.destination.key,
                 ctx.accounts.authority.key,
@@ -28,6 +30,7 @@ pub mod arbitrary_cpi_insecure {
 
 #[derive(Accounts)]
 pub struct Cpi<'info> {
+    // Uses generic AccountInfo, lacking token-specific account validation
     source: AccountInfo<'info>,
     destination: AccountInfo<'info>,
     authority: AccountInfo<'info>,
